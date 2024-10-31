d9840a253409b12ab6da46b0dc3797e202e4a48a
calin-13 <calin.barbu48@gmail.com>
2024-10-31 09:25:02 +0200

    Test-commit
    Am modificat main.cpp

--- ETER_MODERN_C++/ETER_MODERN_C++/main.cpp
+++ ETER_MODERN_C++/ETER_MODERN_C++/main.cpp
@@ -1,9 +1,10 @@
 #include "Game.h"
 
 int main() {
-	// TODO 1: Create a game loop here
+	// TODO 1: Create a game loop herec
 	// Se va creea un joc, se va initializa, se da start play
 	// totul intrun while astfel incat la final sa intrebi jucatorul daca mai doreste sa joace
 	// daca da, resetezi jocul, daca nu iesi din while
+    cout << "Hellow World!" << endl;
 	return 0;
 }
