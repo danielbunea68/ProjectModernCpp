29952e54e1004dc21b4e952c55bfc796ae3f0db5
calin-13 <calin.barbu48@gmail.com>
2024-11-01 20:54:55 +0200

    Update Game.cpp
    Adaugă logica exploziei în Game.cpp, verificând completarea unei linii și coloane și declanșând explozia dacă sunt ocupate complet.

--- ETER_MODERN_C++/ETER_MODERN_C++/Game.cpp
+++ ETER_MODERN_C++/ETER_MODERN_C++/Game.cpp
@@ -47,12 +47,15 @@ void Game::PlayGame()
 		while (!board.CanMakeMove(row, col, chosenCard)) {
 			std::cout << "Enter row and column (0, 1, or 2) to place the card: ";
 			std::cin >> row >> col;
-			board.MakeMove(row, col, chosenCard);
 		}
-        //to do 2
-        //Verific daca este completa o linie si o coloana care se intersecteaza cu ajutorul matricei marked
-        //daca da, atunci trebuie sa exploda bomba
-		if (board.CheckWinner(chosenCard.getColor())) {
+        board.MakeMove(row, col, chosenCard);
+        marked[row][col] = true;
+
+        if (CheckLineAndColumnForExplosion(row, col)) {
+            Explode(row, col);
+        }
+
+        if (board.CheckWinner(chosenCard.getColor())) {
 			board.Display();
 			std::cout << currentPlayer->getName() << " wins!\n";
 			gameOver = true;
@@ -74,4 +77,27 @@ void Game::ResetGame()
 	player1.ClearCards();
 	player2.ClearCards();
 	InitGame();
+}
+
+bool Game::CheckLineAndColumnForExplosion(int row, int col) {
+    bool rowComplete = true;
+    bool colComplete = true;
+
+    for (int i = 0; i < board.GetSize(); ++i) {
+        if (!marked[row][i]) rowComplete = false;
+        if (!marked[i][col]) colComplete = false;
+    }
+    return rowComplete && colComplete;
+}
+
+
+void Game::Explode(int row, int col) {
+    std::cout << "Explosion triggered at (" << row << ", " << col << ")!\n";
+
+    for (int i = 0; i < board.GetSize(); ++i) {
+        board.MakeMove(row, i, Card());
+        board.MakeMove(i, col, Card());
+        marked[row][i] = false;
+        marked[i][col] = false;
+    }
 }
