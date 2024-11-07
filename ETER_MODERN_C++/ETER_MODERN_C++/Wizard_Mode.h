65cf14a54780fe8fc38a28768877dcf3e7accdab
calin-13 <calin.barbu48@gmail.com>
2024-11-08 00:00:05 +0200

    Am implementat clasa Wizard_Mode.h
    Wizard_Mode.h definește structura și funcționalitatile pentru Modul Vrajitor în jocul “Eter”. Fiecare jucator are o putere unica, utilizabila o singura data pe joc, care influenteaza tabla de joc.

--- ETER_MODERN_C++/ETER_MODERN_C++/Wizard_Mode.h
+++ ETER_MODERN_C++/ETER_MODERN_C++/Wizard_Mode.h
@@ -1,10 +1,29 @@
 #pragma once
 #include "Game.h"
-class Wizard_Mode :public Game
-{
+#include "Player.h"
 
-	void ChoseWizard();
-	
-
+enum class WizardPower {
+    RemoveOpponentCard,
+    RemoveRow,
+    CoverOpponentCard,
+    CreatePit,
+    MoveOwnStack,
+    ExtraEterCard,
+    MoveOpponentStack,
+    MoveEdgeRow
 };
 
+class Wizard_Mode : public Game {
+private:
+    void removeOpponentCard(int row, int col);
+    void removeRow(int row);
+    void coverOpponentCard(int row, int col);
+    void createPit(int row, int col);
+    void moveOwnStack(int row, int col);
+    void grantExtraEterCard();
+    void moveOpponentStack(int row, int col);
+    void moveEdgeRow(int row);
+
+public:
+    void activatePower(WizardPower power, int row = -1, int col = -1);
+};
