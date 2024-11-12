7d89631ffb43fc881cd78a13131a11e298ba3599
calin-13 <calin.barbu48@gmail.com>
2024-11-12 20:49:34 +0200

    Am implementat Wizard_Card.h
    reprezintă o carte de vrăjitor cu o abilitate unică utilizabilă o singură dată per joc. Include enum-ul WizardAbility pentru diferite tipuri de abilități și atribute pentru gestionarea activării unice a fiecărei abilități.

---
+++ ETER_MODERN_C++/ETER_MODERN_C++/Wizard_Card.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include "Game.h"
+
+enum class WizardAbility {
+    RemoveOpponentCard,
+    RemoveRow,
+    CoverOpponentCard,
+    CreatePit,
+    MoveOwnStack,
+    ExtraEterCard,
+    MoveOpponentStack,
+    MoveEdgeRow
+};
+
+class Wizard_Card {
+private:
+    WizardAbility ability;
+    bool isUsed;
+
+public:
+    Wizard_Card(WizardAbility abilityType);
+
+    WizardAbility GetAbilityType() const;
+
+    void ActivateAbility(Game* gameInstance, int row = -1, int col = -1);
+
+    bool IsUsed() const;
+};
