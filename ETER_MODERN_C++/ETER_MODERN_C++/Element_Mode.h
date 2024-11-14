f22663ad7c9402b5ff3e81ad7d3bb915a16e1eb3
Palaghia Andrei <andreipalaghiade@gmail.com>
2024-11-14 20:01:25 +0200

    am initializat elemtal mode, am scris functiile pentru jocul de baza ca mai apoi sa adaug logica pentru elemental mode

--- ETER_MODERN_C++/ETER_MODERN_C++/Element_Mode.h
+++ ETER_MODERN_C++/ETER_MODERN_C++/Element_Mode.h
@@ -1,10 +1,11 @@
 ﻿#pragma once
 #include <iostream>
+#include "Game.h"
 
-class Element_Card 
+class Element_Mode :public IGame
 {
 public:
-    enum class Putere 
+    enum class Putere
     {
         ExplozieControlata,
         Distrugere,
@@ -31,13 +32,28 @@ public:
         Avalansa,
         Bolovan
     };
-
 private:
     Putere tipPutere;
+    Board board;
+    Player player1, player2;
+    Player* currentPlayer;
+
+    void SwitchTurn();
 
 public:
 
-    Element_Card(Putere putere);
+
+
+    void InitGame(std::string name1, std::string name2) = 0;
+    void PlayGame() = 0;
+    void ResetGame() = 0;
+
+    void RemoveCard(int row, int col) = 0;
+    void ReturnCardToPlayer(int row, int col) = 0;
+    void CreatePit(int row, int col) = 0;
+
+
+    Element_Mode(Putere putere);
     Putere GetTipPutere() const;
     void ActivatePower() const;
     void ActivateControlledExplosion() const;
