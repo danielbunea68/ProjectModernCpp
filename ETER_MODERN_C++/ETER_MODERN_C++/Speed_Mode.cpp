ee380c883bd8f92a86b5f6d796b583a8c60b4019
calin-13 <calin.barbu48@gmail.com>
2024-12-02 19:33:14 +0200

    Am implementat functiile ramase din clasa SpeedMode

--- ETER_MODERN_C++/ETER_MODERN_C++/Speed_Mode.cpp
+++ ETER_MODERN_C++/ETER_MODERN_C++/Speed_Mode.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <chrono>
 
-
 Speed_Mode::Speed_Mode() : isGameOver(false), currentPlayer(nullptr) {}
 
 Speed_Mode::~Speed_Mode() {}
@@ -36,3 +35,46 @@ void Speed_Mode::CheckWinner() {
     }
 }
 
+void Speed_Mode::PlayGame() {
+    while (!isGameOver) {
+        board.Display();
+        std::cout << currentPlayer->getName() << "'s turn.\n";
+
+        TimerBasedPlay();
+
+        CheckWinner();
+        if (!isGameOver) {
+            SwitchTurn();
+        }
+    }
+}
+
+void Speed_Mode::ResetGame() {
+    board.Clear();
+    player1.ClearCards();
+    player2.ClearCards();
+    InitGame(player1.getName(), player2.getName());
+}
+
+void Speed_Mode::TimerBasedPlay() {
+    using namespace std::chrono;
+    auto start = steady_clock::now();
+
+    int row, col;
+    bool validMove = false;
+
+    do {
+        auto now = steady_clock::now();
+        auto elapsed = duration_cast<seconds>(now - start).count();
+
+        if (elapsed > 10) {
+            std::cout << currentPlayer->getName() << " ran out of time!\n";
+            return;
+        }
+
+        std::cout << "Enter row and column (within 10 seconds): ";
+        std::cin >> row >> col;
+
+        validMove = board.MakeMove(row, col, currentPlayer->PlayCard(0));
+    } while (!validMove);
+}
