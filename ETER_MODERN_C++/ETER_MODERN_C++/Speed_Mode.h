d028740bf2bb2ae0b6daaf15c9e6b818edef4cee
calin-13 <calin.barbu48@gmail.com>
2024-12-02 19:22:34 +0200

    Am creat functia Speed_Mode pentru modul viteza

---
+++ ETER_MODERN_C++/ETER_MODERN_C++/Speed_Mode.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Board.h"
+#include "Player.h"
+#include "IGame.h"
+
+class Speed_Mode : public IGame {
+private:
+    Board board;
+    Player player1, player2;
+    Player* currentPlayer;
+    bool isGameOver;
+
+    void SwitchTurn();
+    void CheckWinner();
+public:
+    Speed_Mode();
+    ~Speed_Mode();
+
+    void InitGame(std::string name1, std::string name2) override;
+    Player* CurrentTurn() override;
+    Player* PreviousTurn() override;
+    void PlayGame() override;
+    void ResetGame() override;
+
+    void TimerBasedPlay();
+};
