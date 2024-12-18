8ae059efa21152be4f5335ddbb061ddfec1d1c32
danielbunea68 <danielbunea6@gmail.com>
2024-12-18 15:21:56 +0200

    am formatat fisierul

--- ETER_MODERN_C++/ETER_MODERN_C++/cardsWidget.h
+++ ETER_MODERN_C++/ETER_MODERN_C++/cardsWidget.h
@@ -7,29 +7,29 @@
 #include "Player.h"
 #include "Card.h"
 #include "QMouseEvent"
-class CardsWidget :public QWidget 
+class CardsWidget :public QWidget
 {
 	Q_OBJECT
-public :
-    explicit CardsWidget(QWidget* parent = nullptr);
+public:
+	explicit CardsWidget(QWidget* parent = nullptr);
 
-    void setCards(Player*player);
+	void setCards(Player* player);
 
 
 protected:
-    void paintEvent(QPaintEvent* event) override;
-    void DrawQueue(QPainter& painter);
-    void DrawCards(QPainter& painter);
-    void mousePressEvent(QMouseEvent* event)override;
+	void paintEvent(QPaintEvent* event) override;
+	void DrawQueue(QPainter& painter);
+	void DrawCards(QPainter& painter);
+	void mousePressEvent(QMouseEvent* event) override;
 
-    void DrawCard(QPainter& painter, int i);
-    void DrawBomb(QPainter& painter, int i);
-private :
+	void DrawCard(QPainter& painter, int i);
+	void DrawBomb(QPainter& painter, int i);
+private:
 
-    int cellWidth{};
-    int cellHeight{};
-    int boardSize{};
+	int cellWidth{};
+	int cellHeight{};
+	int boardSize{};
 
-    Player* player;
+	Player* player;
 };
 
